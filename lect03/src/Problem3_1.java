5d9b384fc0bfa0504a3f6ab31721c5a3c336e9e7
TePattaraton <77090233+TePattaraton@users.noreply.github.com>
2021-01-21 17:57:44 +0700

    Update Problem3_1.java

--- lect03/src/Problem3_1.java
+++ lect03/src/Problem3_1.java
@@ -1,14 +1,33 @@
-// Student ID:
-// First Name:
-// Last Name:
+// Student ID:6230300761
+// First Name:Pattaraton
+// Last Name:Prangprakhon
 //
 // Team Members:
-// 1. <student id> <first name> <last name>
-// 2.
-// 3.
+// 1. 6230300761 Pattaraton Prangprakhon
+// 2. 6230301091 Ittikon Chaipool
+import java.util.Scanner;
 
 public class Problem3_1 {
-    public static void main(String[] args) {
-        // TODO
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+
+        int[] keep = new int[11];
+        int max = 0;
+        System.out.println("Input");
+        for (int i=0; i<keep.length; i++)
+        {
+            int number = input.nextInt();
+            if (number%4 == 0)
+            {
+                keep[i] = number;
+                if (keep[i] > max) 
+                {
+                    max =keep[i];
+                }
+            }
+        }
+        System.out.println("Output");
+        System.out.println(max);
     }
 }
